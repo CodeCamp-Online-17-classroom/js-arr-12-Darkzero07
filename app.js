634bb6742266ac23054cd2970245a1e6adacd19d
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-02 11:27:00 +0700

    js lab 98

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-// write code here
+const cars = ["volvo", "toyota", "honda", "ford"];
+const index = cars.findIndex((value) => value === "honda") +1;
+cars.splice(index, 0, "mazda", "bmw", "nissan");
+console.log(cars);
